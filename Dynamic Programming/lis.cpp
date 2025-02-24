b67218b93ac15c71acc5b90fcec728076290b5a2
Mukul Kumar <mukulkum0121@gmail.com>
2025-02-24 20:33:45 +0530

    All testcases working

---
+++ Dynamic Programming/lis.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int lengthOfLIS(vector<int>& nums) {
+    vector<int> dp;
+    for (int num : nums) {
+        auto it = lower_bound(dp.begin(), dp.end(), num);
+        if (it == dp.end()) {
+            dp.push_back(num);
+        } else {
+            *it = num;
+        }
+    }
+    return dp.size();
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    cout << lengthOfLIS(arr) << endl;
+    return 0;
+}
